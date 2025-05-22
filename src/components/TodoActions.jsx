c31e9f5b2bcad3a7713bdd0e97736cc22da3cb2d
Sanvi Aswal <aswal.sanvi8@gmail.com>
2025-05-23 00:36:30 +0530

    Delete src directory

--- src/components/TodoActions.jsx
+++
@@ -1,9 +0,0 @@
-import "./TodoApp.css";
-
-export default function TodoActions({ onMarkAllAsDone }) {
-  return (
-    <div className="todo-actions">
-      <button onClick={onMarkAllAsDone}>Mark All As Done</button>
-    </div>
-  );
-}
